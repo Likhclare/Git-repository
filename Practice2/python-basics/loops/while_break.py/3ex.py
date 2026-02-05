84fc5fd14cff3931967de85e222dc5bfcdd129bf
Alimov Alikhan <alikhan270307@gmail.com>
2026-02-05 23:25:12 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/python-basics/loops/while_break.py/3ex.py
@@ -0,0 +1,5 @@
+while True:
+    word = input("Enter a word: ")
+    if word == "stop":
+        break
+    print(word)
---
+++ Practice2/python-basics/loops/while_break.py/4ex.py
@@ -0,0 +1,10 @@
+i = 1
+total = 0
+
+while True:
+    total += i
+    if total > 10:
+        break
+    i += 1
+
+print(total)
