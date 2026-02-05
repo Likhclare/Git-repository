4a8375fcce5203cfa0ed46d8083047c0c654fc62
Alimov Alikhan <alikhan270307@gmail.com>
2026-02-05 22:41:08 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ .vscode/f.py
@@ -0,0 +1,5 @@
+a=list(input())
+t=0
+for i in range(0,len(a)-1):
+    t+=a[i]
+print(t)    
---
+++ Practice2/python-basics/Boolean/Boolean_intro.py/5ex.py
@@ -0,0 +1,8 @@
+a=True
+b=3
+while b>0:
+     if a:
+         b-=1
+     else:
+          a=False
+          print("b=0")
