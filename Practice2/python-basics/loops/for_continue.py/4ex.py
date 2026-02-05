a8a0e1d56c73ed65c4fbd242166c9d816c864823
Alimov Alikhan <alikhan270307@gmail.com>
2026-02-05 23:19:11 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/python-basics/loops/for_continue.py/4ex.py
@@ -0,0 +1,6 @@
+words = ["apple", "banana", "skip", "orange"]
+
+for w in words:
+    if w == "skip":
+        continue
+    print(w)
---
+++ Practice2/python-basics/loops/for_continue.py/5ex.py
@@ -0,0 +1,6 @@
+numbers = [3, -1, 4, -2, 5]
+
+for n in numbers:
+    if n < 0:
+        continue
+    print(n)
